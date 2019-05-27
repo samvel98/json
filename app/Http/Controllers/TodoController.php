885003c0b4ad00c8b8a7bf52a4196cdbe17795a4
Samvel <samvel.asatryan.98@gmail.com>
2019-05-27 14:04:02 +0400

    first

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -9,28 +9,28 @@ use App\Todo;
 class TodoController extends Controller
 {
     public function index() {
-    	return response()->json(Todo::all());
+        return response()->json(Todo::all());
     }
 
     public function create(Request $request) {
-    	$todo = Todo::create($request->all());
-    	return response()->json($todo);
+        $todo = Todo::create($request->all());
+        return response()->json($todo);
     }
 
     public function show($id) {
-    	return response()->json(Todo::find($id));
+        return response()->json(Todo::find($id));
     }
 
- 	public function update(Request $request, $id)
- 	{
- 		$todo = Todo::find($id);
-    	$todo->fill($request->all())->save();
-    	return response()->json($todo);
- 	}
+    public function update(Request $request, $id)
+    {
+        $todo = Todo::find($id);
+        $todo->fill($request->all())->save();
+        return response()->json($todo);
+    }
 
     public function destory($id) {
-    	$todo = Todo::find($id);
-    	$todo->delete();
-    	return response()->json(Todo::all());
+        $todo = Todo::find($id);
+        $todo->delete();
+        return response()->json(Todo::all());
     }
 }
